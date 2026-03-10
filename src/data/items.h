9e95f7f8cbbde21777d5d651458f7c09b8fe8bac
Cykada <f.zone513@gmail.com>
2026-03-10 16:45:30 +1100

    Mart Update
    Reworked how the pokemart works (items are based on badge count)
    tweaked Newstem town
    made the route 10 resort a heal spot

--- src/data/items.h
+++ src/data/items.h
@@ -221,7 +221,7 @@ const struct Item gItemsInfo[] =
     [ITEM_MASTER_BALL] =
     {
         .name = _("Master Ball"),
-        .price = 0,
+        .price = 20000,
         .description = COMPOUND_STRING(
             "The best Ball that\n"
             "catches a Pokémon\n"
